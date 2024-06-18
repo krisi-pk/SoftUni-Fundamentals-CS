f75beb6b73a7139b18271c36cb4b51ef1f57dfdd
krisi-k <kristy.kostadinova@gmail.com>
2024-06-18 14:24:55 +0300

    01. Ranking_Problems Descriptions (1).docx- 3. MOBA Challenger
    //dictionary

--- Program.cs
+++ Program.cs
@@ -18,36 +18,92 @@ namespace SoftUniList
         {
 
 
-            //hashset 7
+            //dict 3 
 
-            string command = Console.ReadLine();
-            HashSet<string> parcking = new HashSet<string>();
-            while (!command.Equals("END"))
+            Dictionary<string, Dictionary<string, int>> playerPoints = new Dictionary<string, Dictionary<string, int>>();
+
+            string input = Console.ReadLine();
+            while (!input.Equals("Season end"))
             {
-                string[] tokens = command.Split(new string[] { ", " }, StringSplitOptions.None);
-                string activity = tokens[0];
-                string car = tokens[1];
-                if (activity.Equals("IN"))
+                if (input.Contains("->"))
                 {
-                    parcking.Add(car);
+                    string[] tokens = input.Split(new string[] { " -> " }, StringSplitOptions.None);
+                    string player = tokens[0];
+                    string position = tokens[1];
+                    int skill = Int32.Parse(tokens[2]);
+
+                    if (!playerPoints.ContainsKey(player))
+                    {
+                        playerPoints.Add(player, new Dictionary<string, int>());
+                        playerPoints[player].Add(position, skill);
+                    }
+                    else if (!playerPoints[player].ContainsKey(position))
+                    {
+                        playerPoints[player].Add(position, skill);
+                    }
+                    else if (playerPoints[player][position] < skill)
+                    {
+                        playerPoints[player][position] = skill;
+                    }
                 }
                 else
                 {
-                    parcking.Remove(car);
+                    string[] tokens = input.Split(new string[] { " vs " }, StringSplitOptions.None);
+                    string first = tokens[0];
+                    string second = tokens[1];
+                    if (playerPoints.ContainsKey(first) && playerPoints.ContainsKey(second))
+                    {
+                        int totalPointF = 0;
+                        int totalPointS = 0;
+                        bool fight = false;
+                        foreach (KeyValuePair<string, int> placeF in playerPoints[first])
+                        {
+                            foreach (KeyValuePair<string, int> placeS in playerPoints[second])
+                            {
+                                if (placeF.Key.Equals(placeS.Key))
+                                {
+                                    totalPointF += placeF.Value;
+                                    totalPointS += placeS.Value;
+                                    fight = true;
+                                }
+                            }
+                        }
+                        if (fight == true)
+                        {
+                            if (totalPointF > totalPointS)
+                            {
+                                playerPoints.Remove(second);
+                            }
+                            else if (totalPointS > totalPointF)
+                            {
+                                playerPoints.Remove(first);
+                            }
+                        }
+                    }
                 }
-                command = Console.ReadLine();
+                input = Console.ReadLine();
             }
-            if (parcking.Count <= 0)
+
+            foreach (KeyValuePair<string, Dictionary<string, int>> pl in playerPoints)
             {
-                Console.WriteLine("Parking Lot is Empty");
-            }
-            else {
-                foreach (string c in parcking)
+                int points = 0;
+                foreach (KeyValuePair<string, int> power in pl.Value)
                 {
-                    Console.WriteLine(c);
+                    points = points + power.Value;
+                }
+                Console.WriteLine("{0}: {1} skill", pl.Key, points);
+
+                var orderedPoints =
+                    from po in pl.Value
+                    orderby po.Value descending
+                    select po;
+
+                foreach (var place in orderedPoints)
+                {
+                    Console.WriteLine("- {0} <::> {1}", place.Key, place.Value);
                 }
             }
-            
+
             Console.ReadLine();
         }
     }
